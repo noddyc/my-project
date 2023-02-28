75a2a19075303a191a5481e169a2084cae5a7e0a
jianhe2git <59978308+noddyc@users.noreply.github.com>
2023-02-28 02:11:34 -0500

    temp work socket

--- src/components/Notifications/Notifications.js
+++ src/components/Notifications/Notifications.js
@@ -22,9 +22,44 @@ function Notifications(props) {
     }, []);
 
 
+    useEffect(()=>{
+        props.setNotifications(
+            (prev)=>{
+                let arr = prev.map((e)=>{
+                    
+                    return {...e, viewed: true}
+            })
+                return arr; 
+            }
+        )
+        // because set notifications is async so props.notifications is not updated yet
+        // let mostRecentArr = props.notifications.map((e)=>{return e.id});
+        // console.log(mostRecentArr)
+        try{
+            let data = qs.stringify({
+                'list':auth().id
+              });
+            let config = {
+                method: 'post',
+                url: 'http://localhost:9001/notifications/updateNotifications',
+                headers: { 
+                  'Content-Type': 'application/x-www-form-urlencoded'
+                },
+                data : data
+              };
+              
+            axios(config)
+              .then((response) => {
+                console.log(JSON.stringify(response.data));
+            })
+        }catch(err){
+            console.log(err.message)
+        }
+    }, [])
+
     // uncomment for update in db
     // useEffect(()=>{
-    //     let mostRecentArr = props.notifications.filter((e)=>{return !e.viewed}).map((e)=>{return e.id})
+    //     let mostRecentArr = props.notifications.filter((e)=>{return !e.viewed}).map((e)=>{return e.id});
     //     try{
     //         let data = qs.stringify({
     //             'list':mostRecentArr
@@ -48,28 +83,10 @@ function Notifications(props) {
     // }, [])
 
 
-    
-    useEffect(()=>{
-        return ()=>{
-            props.setNotifications(
-                (prev)=>{
-                    let arr = prev.map((e)=>{
-                        return {...e, viewed:true}
-                })
-                    return arr; 
-                }
-            )
-        }
-
-    }, [])
-
-
-
-
     return (
         <div className="h-screen relative">
         <Navbar notifications={props.notifications} setNotifications={props.setNotifications} socket={props.socket} notifiCount={props.notifiCount} setNotificount={props.setNotificount} 
-       info={props.info} toggleInfo={props.toggleInfo} setToggleInfo={props.setToggleInfo}></Navbar>
+       info={props.info} setInfo={props.setInfo} toggleInfo={props.toggleInfo} setToggleInfo={props.setToggleInfo}></Navbar>
         <div className="flex flex-row h-[calc(100%-80px)] navbarSM:flex navbarSM:flex-col">
               <LeftSideBar></LeftSideBar>
               <div className=' w-full h-[90%] bg-white gap-2 flex flex-col justify-center items-start ml-40 mt-10 mb-10 relative navbarSM:w-full navbarSM:pl-0 navbarSM:pr-0 navbarSM:ml-0'>
@@ -77,7 +94,7 @@ function Notifications(props) {
                 <div className="flex flex-col gap-8 absolute top-20 left-8">
                 {
                     props.notifications.map((item, index)=>{
-                        console.log(item)
+                        // console.log(item)
                         return (<div key={index} className={`flex flex-col border-2 border-black p-2 rounded-lg gap-2 hover:shadow-xl
                          ${!item.viewed?'bg-red-100':'bg-cardBGColor'}`}>
                         {item.message}
@@ -104,6 +121,27 @@ function Notifications(props) {
                                             return ind != index
                                         })
                                     })
+                                    let id = item.id;
+                                    try{
+                                        let data = qs.stringify({
+                                            'id':id
+                                          });
+                                        let config = {
+                                            method: 'post',
+                                            url: 'http://localhost:9001/notifications/deleteNotifications',
+                                            headers: { 
+                                              'Content-Type': 'application/x-www-form-urlencoded'
+                                            },
+                                            data : data
+                                          };
+                                          
+                                        axios(config)
+                                          .then((response) => {
+                                            console.log(JSON.stringify(response.data));
+                                        })
+                                    }catch(err){
+                                        console.log(err.message)
+                                    }
                                 }
                                 }
                                 >Dismiss</button>}
--- src/components/Utils/Navbar.js
+++ src/components/Utils/Navbar.js
@@ -16,6 +16,7 @@ const Navbar = (props) =>{
     const [len, setLen] = useState(props.notifications.filter((e)=>!e.viewed).length)
 
     console.log(props.info.firstname)
+
     useEffect(()=>{
         try{
             console.log(ip)
@@ -32,7 +33,7 @@ const Navbar = (props) =>{
             };
             axios(config).then(
                 (response)=>{
-                    console.log(response.data.timezone)
+                    // console.log(response.data.timezone)
                     props.setInfo(response.data)
                 }
             )            
@@ -71,8 +72,9 @@ const Navbar = (props) =>{
     useEffect(()=>{
         console.log("this is navbar")
         props.socket.off("increaseNotifyCount").on("increaseNotifyCount", (e)=>{
+            console.log(e)
             props.setNotifications((prev)=>[...prev, e])
-            console.log(props.notifications)
+            // console.log(props.notifications)
             localStorage.setItem('count', props.notifiCount);
         })
     }, [props.socket]);
@@ -82,9 +84,9 @@ const Navbar = (props) =>{
         console.log("len updated")
     }, [props.notifications])
 
-    useEffect(()=>{
-        localStorage.setItem('count', props.notifiCount);
-    }, [props.notifiCount])
+    // useEffect(()=>{
+    //     localStorage.setItem('count', props.notifiCount);
+    // }, [props.notifiCount])
 
 
     const navLinkStyles = ({isActive})=>{
@@ -124,10 +126,10 @@ const Navbar = (props) =>{
             <div className={`h-full m-0 px-2 py-0 gap-2 navbarSM:w-full navbarSM:hidden`}>
                 <ul className={`flex m-0 px-2 py-0 gap-2  navbarSM:flex-col navbarSM:w-full`}>
                     {props.info.firstname !== undefined && props.info.lastname !== undefined && props.info.identity != undefined && <div className="flex flex-col justify-center items-center gap-1 w-[126.67px] h-20 pt-3 pb-4 px-0"><span>{props.info.firstname === undefined ?
-                    " ": props.info.firstname} {props.info.lastname}</span><span className="w-[126.67px] h-4 not-italic font-medium text-xs leading-4 text-center tracking-[0.5px]">{props.info.identity}</span></div>}
+                    " ": props.info.firstname.toUpperCase()} {props.info.lastname.toUpperCase()}</span><span className="w-[126.67px] h-4 not-italic font-medium text-xs leading-4 text-center tracking-[0.5px]">{props.info.identity}</span></div>}
                     <div className="flex flex-col justify-center items-center gap-1 w-[126.67px] h-20 pt-3 pb-4 px-0 relative"><i className="material-icons hover:cursor-pointer" style={{fontSize: '36px'}}
                     onClick={bellClickHandler}>notifications</i>
-                    <div className="absolute top-5 right-10 bg-red-700 w-5 h-5 text-xs text-white flex justify-center items-center rounded-lg">{len}</div>
+                    {<div className="absolute top-5 right-10 bg-red-700 w-5 h-5 text-xs text-white flex justify-center items-center rounded-lg">{len}</div>}
                     </div>
                     <div className="flex flex-col justify-center items-center gap-1 w-[126.67px] h-20 pt-3 pb-4 px-0"><img onClick={imgClickHandler} src={require("../../assets/img1.jpeg")} 
                     className="flex flex-row justify-center items-center isolate w-8 h-8 p-0 rounded-2xl" style={{cursor:"pointer", height:"3rem", width:"3rem", borderRadius:"10rem"}}></img></div>
