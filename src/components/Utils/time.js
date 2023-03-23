2cabd1e0df021dc7880ba7f9e0190cdd9ec7a25a
jianhe2git <59978308+noddyc@users.noreply.github.com>
2023-03-23 02:07:35 -0400

    1

--- src/components/Utils/time.js
+++ src/components/Utils/time.js
@@ -1,15 +1,21 @@
 import moment from 'moment'
 
-export const dayStartHour = 18;
-export const dayStartMin = 35;
+
+export const dayStartHourSaving = 6;
+export const dayEndHourSaving = 6;
+
+export const dayStartHour = 7;
+export const dayStartMin = 10;
 export const dayStartSec = 50;
 
-export const dayEndHour = 18;
+export const dayEndHour = 7;
 export const dayEndMin = 40;
 export const dayEndSec = 50;
 
-export const dayStartHourSaving = 17;
-export const dayEndHourSaving = 17;
+
+
+export const nightStartHourSaving = 2;
+export const nightEndHourSaving = 2;
 
 export const nightStartHour = 3;
 export const nightStartMin = 17;
@@ -19,8 +25,6 @@ export const nightEndHour = 3;
 export const nightEndMin = 22;
 export const nightEndSec = 50;
 
-export const nightStartHourSaving = 2;
-export const nightEndHourSaving = 2;
 
 
 export function checkDayLightSaving(){
@@ -43,6 +47,6 @@ export function UTCToCentral(e){
 }
 
 export function CentralToUTC(e){
-    
+
 
 }
